4841a4af7a9007f32df5b9a877a748ff775e7a0e
Mina.Dadashi <Mina.Dadashi@IT-N-DADASHI>
2024-03-11 15:03:26 +0330

    Add project files.

---
+++ DapperExample.Api/Controllers/UserController.cs
@@ -0,0 +1,72 @@
+﻿using DapperExample.DataAccess.Repositories;
+using DapperExample.Models.DTOs;
+using DapperExample.Models.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DapperExample.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        #region Fields
+
+        private readonly IUserRepository _userRepository;
+
+        #endregion
+
+        #region Ctor
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        #endregion
+
+        #region Method [s]
+
+        [HttpGet]
+        public async Task<List<User>> GetAll()
+        {
+            return await _userRepository.GetAll();
+        }
+
+        [HttpGet]
+        [Route("GetById/{id:int}")]
+        public async Task<User> GetById(int id)
+        {
+            return await _userRepository.GetById(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            await _userRepository.Post(signUpDto);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("Update/{id:int}")]
+        public async Task<IActionResult> Update(int id, UpdateDto updateDto)
+        {
+            await _userRepository.Update(id, updateDto);
+
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("GetUserRoles/{id:int}")]
+        public async Task<IActionResult> GetUserRoles(int id)
+        {
+            return Ok(await _userRepository.GetUserAndUserRoles(id));
+        }
+
+        #endregion
+
+    }
+}
---
+++ DapperExample.Web/Program.cs
@@ -0,0 +1,78 @@
+﻿using DapperExample.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
+using MassTransit;
+using Quartz;
+using GettingStarted;
+using DapperExample.DataAccess.Context;
+using DapperExample.DataAccess.Jobs;
+
+var builder = WebApplication.CreateBuilder(args);
+IConfiguration _configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+builder.Services.AddQuartz(a =>
+{
+    var jobKey = new JobKey("GetAllJob");
+
+    a.AddJob<GetAllJob>(a => a.WithIdentity(jobKey));
+
+    a.UseMicrosoftDependencyInjectionJobFactory();
+
+    a.AddTrigger(a => a
+        .ForJob(jobKey)
+        .StartNow()
+        .WithIdentity("GetAllJob-trigger")
+        .WithSimpleSchedule(b => b.WithIntervalInSeconds(30).RepeatForever()));
+
+});
+
+
+builder.Services.AddQuartzHostedService(a => a.WaitForJobsToComplete = true);
+
+builder.Services.AddMassTransit(a =>
+{
+
+    a.AddConsumer<MessageConsumer>();
+
+    a.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+//builder.Services.AddHostedService<Worker>();
+
+
+
+builder.Services.AddDbContext<SqlDataAccess>(options =>
+{
+    options.UseSqlServer(_configuration.GetConnectionString("DefaultConnectionStrings"));
+});
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
